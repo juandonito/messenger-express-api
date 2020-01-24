ec4fd4d089665d1c9db8ce9e619c5ae4cce1c547
anguadi <anguadir@gmail.com>
2020-01-24 17:15:25 +0100

    deleted get user route

--- app/users/index.js
+++ app/users/index.js
@@ -5,9 +5,6 @@ import * as userController from './userController'
 const usersRouter = express.Router()
 
 usersRouter.route('/')
-	.get((req, res) => {
-		res.sendStatus(200)
-	})
 	.post(userController.postUser)
 
 export default usersRouter

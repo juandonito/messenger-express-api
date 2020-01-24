1bf3123221c7c5d60ffc5a9108baa1eaa72417f4
anguadi <anguadir@gmail.com>
2020-01-24 11:53:22 +0100

    first commit

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended : true }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+	res.send({
+		message : 'Api running'
+	})
+})
+
+module.exports = app
